73a8dfead04cf95494c481c47ded7129e881bb6d
jslupicki <YwUyezN3>
2022-10-13 14:12:41 +0200

    Switch to TS

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,45 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+<script lang="ts">
+	export let name: string;
+	export let surname: string;
+	export let counter: number = 0;
+	let color = "red";
+
+	function increment() {
+		counter += 1;
+		console.log(`Set counter to ${counter}`)
+	}
+	function decrement() {
+		counter -= 1;
+		console.log(`Set counter to ${counter}`)
+	}
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+	<h1>Hello {name} {surname} {counter}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={increment}>+</button>
+	<button on:click={decrement}>-</button>
+	<h2 on:mouseenter={() => color = "red"} on:mouseleave={() => color = "green"} style="color: {color};">HELLO</h2>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
 </style>
