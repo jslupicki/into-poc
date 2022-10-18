fd08605b631557c747ec3a9324cddefd0c88e62e
Jan Slupicki <jan.slupicki-ext@seba.swiss>
2022-10-18 13:31:25 +0200

    Clean up CSS

--- src/App.svelte
+++ src/App.svelte
@@ -25,16 +25,4 @@
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
