deb50a1a1b3f6f7e5c14b853a921583113e0993d
Jan Slupicki <jan.slupicki-ext@seba.swiss>
2022-10-19 13:30:56 +0200

    Use settings in frontend

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,50 @@
 <script lang="ts">
-	import {logged} from './lib/storages.js'
-	import MainPage from './lib/Main.svelte';
-	import LoginPage from './lib/Login.svelte';
-	import BottomAppBar, {
-    	Section,
-    	AutoAdjust,
-	} from '@smui-extra/bottom-app-bar';
-	import logo from './assets/images/seba_logo.svg';
+    import {logged} from './lib/storages.js'
+    import MainPage from './lib/Main.svelte';
+    import LoginPage from './lib/Login.svelte';
+    import BottomAppBar, {
+        Section,
+        AutoAdjust,
+    } from '@smui-extra/bottom-app-bar';
+    import logo from './assets/images/seba_logo.svg';
+    import {onMount} from "svelte";
 
-	let bottomAppBar: BottomAppBar;
-	let page = LoginPage
-	let loggedValue
+    let bottomAppBar: BottomAppBar;
+    let page = LoginPage
+    let loggedValue
+	let settings = {}
 
-	const unsubscribe = logged.subscribe(value => {
-		loggedValue = value;
-	});
+    const loggedSubscription = logged.subscribe(value => {
+        loggedValue = value;
+    });
 
-	function loggedEventArrive(event) {
-		page = MainPage
-		console.log('Got logged: ' + event.detail.login)
-	}
+    function loggedEventArrive(event) {
+        page = MainPage
+        console.log('Got logged: ' + event.detail.login)
+    }
 
+    onMount(async () => {
+        fetch("/api/Settings")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                settings = data;
+            }).catch(error => {
+            console.log(error);
+            return [];
+        });
+    });
 </script>
 
 <div>
-	<img src={logo}/>
-	<AutoAdjust {bottomAppBar}>
-		<svelte:component this={page} on:logged={loggedEventArrive}/>
-	</AutoAdjust>
+    <img src={logo}/>
+    <AutoAdjust {bottomAppBar}>
+        <svelte:component this={page} on:logged={loggedEventArrive}/>
+    </AutoAdjust>
 
-	<BottomAppBar bind:this={bottomAppBar}>
-		<Section>
-			User {loggedValue.login}
-		</Section>
-	</BottomAppBar>
+    <BottomAppBar bind:this={bottomAppBar}>
+        <Section>
+            User {loggedValue.login} Settings: {settings}
+        </Section>
+    </BottomAppBar>
 </div>
