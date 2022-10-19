0e1dec2479ce597b00f8d11e4114b8319dda132f
Jan Slupicki <jan.slupicki-ext@seba.swiss>
2022-10-19 11:48:23 +0200

    Settings continue

--- api/Settings/index.js
+++ api/Settings/index.js
@@ -4,13 +4,9 @@ module.exports = async function (context, req) {
     try {
         let testSetting = process.env.test
     } catch (error) {
-        let testSetting = error
+        let testSetting = "unknown"
     }
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = (name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-        ) + "  TestSetting = " + testSetting;
+    const responseMessage = "TestSetting = " + testSetting;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
