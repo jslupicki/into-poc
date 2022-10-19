a32875e8380650d9386c0eef48051c168c159a89
Jan Slupicki <jan.slupicki-ext@seba.swiss>
2022-10-19 12:43:13 +0200

    Settings

--- api/Settings/index.js
+++ api/Settings/index.js
@@ -1,16 +1,8 @@
 module.exports = async function (context, req) {
     let contextString = JSON.stringify(context)
     let reqString = JSON.stringify(req)
-    let processString = JSON.stringify(process)
-    
-    try {
-        let testSetting = process.env.test
-    } catch (e) {
-        console.log(e)
-        let testSetting = "error"
-    }
 
-   const responseMessage = "test = " + testSetting + ", process = " + processString + ", req = " + reqString + ", context = " + contextString;
+   const responseMessage = "req = " + reqString + ", context = " + contextString;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
