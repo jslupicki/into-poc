4e27a4bfae5c5be52abfdd70ec71a6b183a4bfce
Jan Slupicki <jan.slupicki-ext@seba.swiss>
2022-10-19 12:02:13 +0200

    Settings

--- api/Settings/index.js
+++ api/Settings/index.js
@@ -1,12 +1,9 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    try {
-        let testSetting = process.env.test
-    } catch (error) {
-        let testSetting = "unknown"
-    }
-    const responseMessage = "TestSetting = " + testSetting;
+   let testSetting = "unknown"
+
+   const responseMessage = "TestSetting = " + testSetting;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
