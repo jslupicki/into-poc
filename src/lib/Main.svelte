f26149c50de35a400bfcb5cb1a057a5c28be1549
Jan Slupicki <jan.slupicki-ext@seba.swiss>
2022-10-19 08:25:10 +0200

    Set up menu on Main page.

--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -1,6 +1,54 @@
 <script lang="ts">
+    import Drawer, {AppContent, Content} from '@smui/drawer';
+    import List, {Item, Text} from '@smui/list';
+    import survey from "./survey/Survey.svelte"
+    import upload from "./upload/Upload.svelte"
+
+    let page = survey;
 </script>
 
-<div>
-    Main Page
+<div class="drawer-container">
+    <Drawer>
+        <Content>
+            <List>
+                <Item on:click={() => (page = survey)} >
+                    <Text>Survey</Text>
+                </Item>
+                <Item on:click={() => (page = upload)} >
+                    <Text>Upload files</Text>
+                </Item>
+            </List>
+        </Content>
+    </Drawer>
+
+    <AppContent class="app-content">
+        <main class="main-content">
+            <svelte:component this={page}/>
+        </main>
+    </AppContent>
 </div>
+
+<style>
+    /* These classes are only needed because the
+      drawer is in a container on the page. */
+    .drawer-container {
+        position: relative;
+        display: flex;
+        overflow: hidden;
+        z-index: 0;
+    }
+
+    * :global(.app-content) {
+        flex: auto;
+        overflow: auto;
+        position: relative;
+        flex-grow: 1;
+    }
+
+    .main-content {
+        overflow: auto;
+        padding: 16px;
+        height: 100%;
+        box-sizing: border-box;
+    }
+</style>
---
+++ src/lib/upload/Upload.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div>
+    Upload
+</div>
